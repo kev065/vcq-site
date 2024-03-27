ccc5aa525a8e1e69ce12688370a332b5fc6e1af3
KELVIN MURITHI <kelvinmurithi@live.com>
2024-03-27 14:11:17 +0300

    Adjust text width in hero section
    - This commit introduces a new CSS class 'hero-paragraph' to control the maximum width of the text in the hero section of the Vcq component.
    - This ensures that the text doesn't span the entire width of the page, improving readability and aesthetics.

--- client/src/components/Vcq.js
+++ client/src/components/Vcq.js
@@ -10,9 +10,9 @@ const Vcq = () => {
         </div>
         <nav>
           <ul>
-          <li><button className="link-button">Exchanges</button></li>
+          {/*<li><button className="link-button">Exchanges</button></li>
           <li><button className="link-button">Products</button></li>
-          <li><button className="link-button">About</button></li>
+          <li><button className="link-button">About</button></li>*/}
           </ul>
         </nav>
         {/*<button className="sign-in">Sign In</button>*/}
@@ -20,16 +20,14 @@ const Vcq = () => {
 
       <div className="hero">
         <h1>Valcheq – Your News-Powered Trading Edge</h1>
-        <p>In today's fast-paced crypto market, staying ahead of the curve is crucial. Our innovative trading terminal puts the power of real-time news and 
-	seamless execution at your fingertips. </p>
-        <p>Here's what sets us apart:</p>
-        <p>Actionable News Feed: Our curated news feed filters out the noise, highlighting actionable insights that can inform your trading decisions.</p>
-        <p>All-in-one Interface: No more switching tabs! Execute trades directly on your chosen exchange through our secure and integrated platform.</p>
-        <p>Exchange Choice: We connect to a wide range of top crypto exchanges, giving you the freedom to choose the one that best suits your needs.</p>
+        <p className="hero-paragraph">In today's fast-paced crypto market, staying ahead of the curve is crucial. Our innovative trading terminal 
+          puts the power of real-time news and 
+          seamless execution at your fingertips. </p>
+        <p className="hero-paragraph">Here's what sets us apart:</p>
+        <p className="hero-paragraph">Actionable News Feed: Our curated news feed filters out the noise, highlighting actionable insights that can inform your trading decisions.</p>
+        <p className="hero-paragraph">All-in-one Interface: No more switching tabs! Execute trades directly on your chosen exchange through our secure and integrated platform.</p>
+        <p className="hero-paragraph">Exchange Choice: We connect to a wide range of top crypto exchanges, giving you the freedom to choose the one that best suits your needs.</p>
         <div className="cta-buttons">
-
-          {/*<button className="open-account">Open Account</button>*/}
-
           <a href="mailto:kelvin@valcheq.com" className="contact-us">Contact Us</a>
         </div>
       </div>
