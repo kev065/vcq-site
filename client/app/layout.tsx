4f8e6b22f7f4c4cb01408dc5186b2699384d1adb
Murithi <kelvinmurithi@live.com>
2025-01-02 13:34:53 +0300

    add a 'providers' component to wrap the entire app

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import "./globals.css"
 import Header from "./components/header"
 import Footer from "./components/footer"
+import { Providers } from "./providers"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main className="min-h-screen">{children}</main>
-        <Footer />
+        <Providers>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
