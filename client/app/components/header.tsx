99b9e86122cfc70aaaa117fcba7a6cd9008b96a5
Murithi <kelvinmurithi@live.com>
2025-01-02 11:24:37 +0300

    Remove the text 'Valcheq Technologies' from logo

--- client/app/components/header.tsx
+++ client/app/components/header.tsx
@@ -10,11 +10,10 @@ export default function Header() {
           <Image
             src="/images/valcheq_logo.png"
             alt="Valcheq Technologies Logo"
-            width={32}
-            height={32}
+            width={1918}
+            height={314}
             className="h-8 w-auto"
           />
-          <span className="text-xl font-bold text-gray-800">Valcheq Technologies</span>
         </Link>
         <nav>
           <ul className="flex space-x-4">
