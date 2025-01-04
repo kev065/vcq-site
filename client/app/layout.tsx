04e486dfffdb221426f9203df69950a2e31a8c02
Murithi <kelvinmurithi@live.com>
2025-01-04 10:57:50 +0300

    Implement google analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from "next"
-import { Inter } from 'next/font/google'
-import "./globals.css"
-import Header from "./components/header"
-import Footer from "./components/footer"
-import { Providers } from "./providers"
-import BackToTop from "./components/back-to-top"
+import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+import "./globals.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Providers } from "./providers";
+import BackToTop from "./components/back-to-top";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Valcheq Technologies",
-  description: "Empowering businesses with cutting-edge web development, data analytics, and AI solutions. Valcheq Technologies offers custom website and app development, data analysis services, chatbots and AI-powered applications.",
-}
+  description:
+    "Empowering businesses with cutting-edge web development, data analytics, and AI solutions. Valcheq Technologies offers custom website and app development, data analysis services, chatbots and AI-powered applications.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -27,8 +29,9 @@ export default function RootLayout({
           <Footer />
           <BackToTop />
         </Providers>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string} />
       </body>
     </html>
-  )
+  );
 }
 
